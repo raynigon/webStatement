e7610af0245bb4314f1d73fc164c3733cc803c71
woxxel <alexander.wollex.schmidt@googlemail.com>
2020-12-09 17:33:48 +0100

    included references

--- page/assets/scripts/App.js
+++ page/assets/scripts/App.js
@@ -3,9 +3,7 @@ import generateList from './modules/generateList'
 import generateStatement from './modules/generateStatement'
 
 new generateList('authors',"#list__authors");
-// new generateList('references',"#list__references",
-//                   ["ref__number","ref__authors","ref__year",
-//                   "ref__title","ref__journal"]);
+new generateList('references',"#list__references");
 new generateStatement();
 // new generateStatement('signatories',"#list__signatories");
 
--- page/assets/scripts/modules/generateList.js
+++ page/assets/scripts/modules/generateList.js
@@ -1,7 +1,6 @@
 import Papa from 'papaparse';
 
-function arrayToTable(tableData) {
-    console.log(tableData)
+function csvToList_authors(tableData) {
       var author_all = $("<p></p>");
       $(tableData).each(function (i,rowData) {
           var author = $('<a class="author__name" title="' + rowData.Affiliation + '" href="#">' + rowData.Name + ', </a>');
@@ -10,20 +9,55 @@ function arrayToTable(tableData) {
       return author_all;
 }
 
+function csvToList_references(tableData) {
+      var reference_all = $('<ul style="list-style: none;"></ul>');
+      $(tableData).each(function (i,rowData) {
+        // console.log(rowData)
+          var reference = $('<li></li>');
+          reference.append(
+            $('<span class="ref ref__number">['+rowData.Number+'] </span>')
+          )
+          reference.append(
+            $(' <span class="ref ref__authors">'+rowData.Authors+', </span>')
+          )
+          reference.append(
+            $(' <span class="ref ref__year">'+rowData.Year+', </span>')
+          )
+          reference.append(
+            $(' <span class="ref ref__title">'+rowData.Title+', </span>')
+          )
+          reference.append(
+            $(' <span class="ref ref__year">'+rowData.Journal+'</span>')
+          )
+          // console.log(reference)
+          reference_all.append(reference)
+      });
+      // console.log(reference_all)
+      return reference_all;
+}
+
 class generateList {
 
   constructor(type,dom_ref) {
     var data_file = './data/' + type + '.csv'
-    console.log(data_file)
+    if (type=='authors') var csvToList = csvToList_authors;
+    else if (type=='references') var csvToList = csvToList_references;
+    else if (type=='signatories') var csvToList = csvToList_signatories;
+    console.log(type)
+    console.log(dom_ref)
+    console.log(csvToList)
     $.ajax({
         type: "GET",
         url: data_file,
         success: function (data) {
-          $(dom_ref).html(
-            arrayToTable(Papa.parse(data, {
-              delimiter: ";",
-              header: true,
-              skipEmptyLines: true}).data))
+            $(dom_ref).html(
+                  csvToList(Papa.parse(data, {
+                        delimiter: ";",
+                        header: true,
+                        skipEmptyLines: true,
+                      }).data),
+            console.log('success')
+            )
         }
     });
   }

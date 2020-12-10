3d400f1575d25322feadc7d77fb1449cfd5ff9cc
woxxel <alexander.wollex.schmidt@googlemail.com>
2020-12-10 12:53:29 +0100

    trying to apply patch....

--- page/assets/scripts/App.js
+++ page/assets/scripts/App.js
@@ -12,3 +12,9 @@ new generateStatement();
 if (module.hot) {
   module.hot.accept()
 }
+
+// $(".jumbotron").css({ height: $(window).height() + "px" });
+//
+// $(window).on("resize", function() {
+//   $(".jumbotron").css({ height: $(window).height() + "px" });
+// });
